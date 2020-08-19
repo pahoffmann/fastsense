e9baf63e973a3c4b06ba86d5957cfc2939e02e8a
gjulian <gjulian@uos.de>
2020-08-19 17:02:53 +0200

    separate test makefile and dir

--- test/phidgets/test.cpp
+++ test/phidgets/test.cpp
@@ -2,13 +2,14 @@
 #include "../../src/driver/phidgets/msg/imu_msg.h"
 #include "../../src/util/concurrent_ring_buffer.h"
 
-using Buffer = ConcurrentRingBuffer<ImuMsg>;
+using namespace fastsense::driver;
+using Buffer = ConcurrentRingBuffer<msg::ImuMsg>;
 
 int main() {
     Buffer imu_buffer(1000);
-    phidgets::Imu imu = phidgets::Imu(imu_buffer);
+    Imu imu = Imu(imu_buffer);
     while (true) {
-        ImuMsg msg;
+        msg::ImuMsg msg;
         imu_buffer.pop(&msg);
         std::cout << msg;
     };
