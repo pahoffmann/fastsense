b10b51535c0e3bb85d5e57c885685e8528f6b271
Marcel Flottmann <mflottmann@uos.de>
2020-09-03 00:55:39 +0200

    Logger implementation

---
+++ src/util/logging/sink.h
@@ -0,0 +1,45 @@
+/**
+ * @file sink.h
+ * @author Marcel Flottmann
+ * @date 2020-09-02
+ */
+
+#pragma once
+
+#include <string>
+#include <fstream>
+
+namespace FastSense::util::logging::sink
+{
+
+class Sink
+{
+public:
+    Sink() = default;
+    Sink(Sink&) = delete;
+    Sink& operator=(Sink&) = delete;
+    virtual ~Sink() = default;
+
+    virtual void write(const std::string& msg) = 0;
+};
+
+class CoutSink : public Sink
+{
+public:
+    ~CoutSink() = default;
+
+    void write(const std::string& msg) override;
+};
+
+class FileSink : public Sink
+{
+private:
+    std::ofstream file;
+public:
+    explicit FileSink(const std::string& filename);
+    ~FileSink() = default;
+
+    void write(const std::string& msg) override;
+};
+
+}
