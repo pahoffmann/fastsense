d08e5b61e521c5deb3887e439839a2ee939e8bbb
gjulian <gjulian@uos.de>
2020-09-06 17:05:10 +0200

    add more make test targets

--- scripts/test_before_push.sh
+++ scripts/test_before_push.sh
@@ -4,24 +4,44 @@ X_PATH="/media/julian/ssdext/xilinx"
 X_VERSION="2019.2"
 ROS_VERSION="melodic"
 
+. "${X_PATH}/Vitis/${X_VERSION}/settings64.sh" && . "/opt/ros/${ROS_VERSION}/setup.bash" && . /opt/xilinx/xrt/setup.sh &> /dev/null
+
 echo "-- Test target 'software'"
-. "${X_PATH}/Vitis/${X_VERSION}/settings64.sh" && make clean software &> /dev/null
+make clean_software software &> /dev/null
 
 if [ ! $? -eq 0 ]; then echo "FAIL" && exit 1; fi
 echo "   done"
 
 sleep 1
 
-echo "-- Test target 'test'"
-. "${X_PATH}/Vitis/${X_VERSION}/settings64.sh" && . /opt/xilinx/xrt/setup.sh &> /dev/null && make test HW_TARGET=sw_emu &> /dev/null
+echo "-- Test target 'test_sensor_sync'"
+make test_sensor_sync HW_TARGET=sw_emu &> /dev/null
 
 if [ ! $? -eq 0 ]; then echo "FAIL" && exit 1; fi
 echo "   done"
 
 sleep 1
+
+echo "-- Test target 'test_zmq_client'"
+make test_zmq_client HW_TARGET=sw_emu &> /dev/null
+
+if [ ! $? -eq 0 ]; then echo "FAIL" && exit 1; fi
+echo "   done"
+
+sleep 1
+
+echo "-- Test target 'test_hdf5'"
+make test_hdf5 HW_TARGET=sw_emu &> /dev/null
+
+if [ ! $? -eq 0 ]; then echo "FAIL" && exit 1; fi
+echo "   done"
+
+sleep 1
+
+
 
 echo "-- Test ROS test nodes"
-. "/opt/ros/${ROS_VERSION}/setup.bash" && cd test && rm -rf build/ devel/ && catkin_make &> /dev/null
+make clean_ros_nodes ros_test_nodes &> /dev/null
 
 if [ ! $? -eq 0 ]; then echo "FAIL" && exit 1; fi
 echo "   done"
