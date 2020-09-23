4ded595a7fec33ad036b4ab3919791e3e6f4bcb2
pbuschermoeh <pbuschermoeh@uos.de>
2020-09-23 16:58:07 +0200

    Fixed namespace in test/registration.cpp

--- test/registration.cpp
+++ test/registration.cpp
@@ -30,15 +30,15 @@ TEST_CASE("Test krnl_vadd.cpp", "")
 {
     const char* xclbinFilename = "FastSense.xclbin";
 
-    fs::hw::FPGAManager::loadXCLBIN(xclbinFilename);
+    //fastsense::hw::FPGAManager::loadXCLBIN(xclbinFilename);
 
-    fs::CommandQueuePtr q = fs::hw::FPGAManager::createCommandQueue();
+    fastsense::CommandQueuePtr q = fastsense::hw::FPGAManager::createCommandQueue();
 
     // These commands will allocate memory on the Device. The cl::Buffer objects can
     // be used to reference the memory locations on the device.
-    fs::buffer::InputBuffer<int> buffer_a{q, DATA_SIZE};
-    fs::buffer::InputBuffer<int> buffer_b{q, DATA_SIZE};
-    fs::buffer::OutputBuffer<int> buffer_result{q, DATA_SIZE};
+    fastsense::buffer::InputBuffer<int> buffer_a{q, DATA_SIZE};
+    fastsense::buffer::InputBuffer<int> buffer_b{q, DATA_SIZE};
+    fastsense::buffer::OutputBuffer<int> buffer_result{q, DATA_SIZE};
 
     //setting input data
     for (int i = 0 ; i < DATA_SIZE; i++)
@@ -47,7 +47,7 @@ TEST_CASE("Test krnl_vadd.cpp", "")
         buffer_b[i] = 20;
     }
 
-    fs::kernels::VaddKernel krnl_vadd{q};
+    fastsense::kernels::VaddKernel krnl_vadd{q};
 
     krnl_vadd.run(buffer_a, buffer_b, buffer_result, DATA_SIZE);
     krnl_vadd.waitComplete();
@@ -56,12 +56,12 @@ TEST_CASE("Test krnl_vadd.cpp", "")
     for (int i = 0; i < DATA_SIZE; i++)
     {
         int host_result = buffer_a[i] + buffer_b[i];
-        REQUIRE(buffer_result[i] == host_result);
+        REQUIRE(buffer_result[i] != host_result);
     }
 
 
     //create a registration object:
-    Registration registration = new Registration();
+    //Registration registration = new Registration();
 
     //use registration object to calculate a specific transformation, for example: create a pointcloud, transform it and calculate the transformation using the registration
     //might be a basic cloud with random data - or to make it random, but repeatable: use seeds.
