16d02ad19e1e06024cabc8aa569f51dabfe85dc6
meisoldt <meisoldt@uni-osnabrueck.de>
2020-09-23 15:50:19 +0200

    Small Change

--- test/tsdf_values.cpp
+++ test/tsdf_values.cpp
@@ -10,10 +10,19 @@
 //#define CATCH_CONFIG_MAIN
 #include "catch2_config.h"
 #include <tsdf/ProjectionNormal.h>
+#include <iostream>
 
 using namespace fastsense::tsdf;
 
 TEST_CASE("Test TSDF Values", "[]")
 {
-    
+    SECTION("Simple cases")
+    {
+        // TODO
+    }
+
+    SECTION("Complete scan")
+    {
+        // TODO
+    }
 }
