d9b6789c3045870a4282a0dccff8a5bebc3c98d5
meisoldt <meisoldt@uos.de>
2020-10-02 15:19:01 +0200

    Add TSDF Hardware template

---
+++ src/hw/kernels/tsdf_kernel.h
@@ -0,0 +1,50 @@
+#pragma once
+
+/**
+ * @author Marc Eisoldt
+ */
+
+#include <hw/kernels/base_kernel.h>
+#include <hw/buffer/buffer.h>
+#include <map/local_map.h>
+#include <iostream>
+
+namespace fastsense::kernels
+{
+
+class LocalMapTestKernel : public BaseKernel
+{
+public:
+    LocalMapTestKernel(const CommandQueuePtr& queue)
+        : BaseKernel{queue, "krnl_tsdf"}
+    {}
+
+    ~LocalMapTestKernel() = default;
+
+    void run(map::LocalMap& map)
+    {
+        resetNArg();
+        setArg(map.getBuffer().getBuffer());
+        auto m = map.getHardwareRepresentation();
+        setArg(m.sizeX);
+        setArg(m.sizeY);
+        setArg(m.sizeZ);
+        setArg(m.posX);
+        setArg(m.posY);
+        setArg(m.posZ);
+        setArg(m.offsetX);
+        setArg(m.offsetY);
+        setArg(m.offsetZ);
+
+        // Write buffers
+        cmd_q_->enqueueMigrateMemObjects({map.getBuffer().getBuffer()}, CL_MIGRATE_MEM_OBJECT_DEVICE, nullptr, &pre_events_[0]);
+
+        // Launch the Kernel
+        cmd_q_->enqueueTask(kernel_, &pre_events_, &execute_events_[0]);
+
+        // Read buffers
+        cmd_q_->enqueueMigrateMemObjects({map.getBuffer().getBuffer()}, CL_MIGRATE_MEM_OBJECT_HOST, &execute_events_, &post_events_[0]);
+    }
+};
+
+} // namespace fastsense::kernels
---
+++ src/tsdf/krnl_tsdf.cpp
@@ -0,0 +1,70 @@
+/**
+ * @author Marc Eisoldt
+ */
+
+#include <tsdf/tsdf_hw.h>
+#include <map/local_map_hw.h>
+
+struct Point
+{
+    int x;
+    int y;
+    int z;
+};
+
+struct IntTuple
+{
+    int first;
+    int second;
+};
+
+extern "C"
+{
+
+    void krnl_tsdf(IntTuple* mapData,
+                             int sizeX,
+                             int sizeY,
+                             int sizeZ,
+                             int posX,
+                             int posY,
+                             int posZ,
+                             int offsetX,
+                             int offsetY,
+                             int offsetZ
+                            )
+    {
+#pragma HLS DATA_PACK variable=mapData
+#pragma HLS INTERFACE m_axi port=mapData offset=slave bundle=gmem
+#pragma HLS INTERFACE s_axilite port=mapData bundle=control
+#pragma HLS INTERFACE s_axilite port=sizeX bundle=control
+#pragma HLS INTERFACE s_axilite port=sizeY bundle=control
+#pragma HLS INTERFACE s_axilite port=sizeZ bundle=control
+#pragma HLS INTERFACE s_axilite port=posX bundle=control
+#pragma HLS INTERFACE s_axilite port=posY bundle=control
+#pragma HLS INTERFACE s_axilite port=posZ bundle=control
+#pragma HLS INTERFACE s_axilite port=offsetX bundle=control
+#pragma HLS INTERFACE s_axilite port=offsetY bundle=control
+#pragma HLS INTERFACE s_axilite port=offsetZ bundle=control
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+
+        fastsense::map::LocalMapHW map{sizeX, sizeY, sizeZ,
+                                       posX, posY, posZ,
+                                       offsetX, offsetY, offsetZ};
+
+        for (int i = map.posX - map.sizeX / 2; i <= map.posX + map.sizeX / 2; i++)
+        {
+            for (int j = map.posY - map.sizeY / 2; j <= map.posY + map.sizeY / 2; j++)
+            {
+                for (int k = map.posZ - map.sizeZ / 2; k <= map.posZ + map.sizeZ / 2; k++)
+                {
+#pragma HLS PIPELINE
+                    IntTuple tmp = map.get(mapData, i, j, k);
+                    tmp.first *= 2;
+                    tmp.second /= 2;
+                    map.set(mapData, i, j, k, tmp);
+                }
+            }
+        }
+    }
+
+}
