a9d12191c301f81530a38e64d442c2dc66268d34
Juri Vana <jvana@uos.de>
2020-10-13 17:29:23 +0200

    Added concurrent ring buffer test

--- test/concurrent_ringbuffer.cpp
+++ test/concurrent_ringbuffer.cpp
@@ -1,36 +1,107 @@
 /**
- * @file hw_buffer.cpp
- * @author Julian Gaal
- * @date 2020-09-28
+ * @file concurrent_ringbuffer.cpp
+ * @author Juri Vana
+ * @date 2020-10-13
  */
 
 #include "catch2_config.h"
-#include "Application.h"
-#include <util/config/config_manager.h>
-#include <util/logging/logger.h>
-#include <msg/imu_msg.h>
+#include <util/concurrent_ring_buffer.h>
+#include <iostream>
+#include <thread>
 
-using namespace fastsense;
-using namespace fastsense::util::config;
-using namespace fastsense::msg;
+using namespace fastsense::util;
 
-TEST_CASE("Test Buffer STL Container", "[]")
+TEST_CASE("ConcRingBuffer", "[ConcRingBuffer]")
 {
-    std::cout << "Testing 'Test Buffer STL Container'" << std::endl;   
+    std::cout << "Testing 'Concurrent Ring Buffer'" << std::endl;
     constexpr size_t buffer_size = 5;
-    data::ImuStampedBufferPtr imuBuffer{std::make_shared<data::ImuStampedBuffer>(buffer_size)};
-    
-    for (size_t i = 0; i < buffer_size*2; i++)
+
+    ConcurrentRingBuffer<int> crb(buffer_size);
+
+    // test getLength and push
+    REQUIRE(crb.getLength() == 0);
+
+    for (uint i = 0; i < buffer_size; i++)
     {
-        ImuMsg imu_msg{};
-        imu_msg.acc += 1;
-        imu_msg.ang += 2;
-        imu_msg.mag += 3;
+        crb.push(i);
     }
 
-    SECTION("Test #1")
-    {
-        std::cout << "    Section 'Test #1'" << std::endl;
+    REQUIRE(crb.getLength() == buffer_size);
 
+    SECTION("Test push_nb, pop, and pop_nb")
+    {
+        // test push_nb, pop
+        REQUIRE(!crb.push_nb(0));
+        REQUIRE(crb.push_nb(buffer_size, true));
+
+        int val;
+        crb.pop(&val);
+        REQUIRE(val == 1);
+        REQUIRE(crb.getLength() == buffer_size - 1);
+
+        // test pop_nb
+        for (int i = 2; i <= (int) buffer_size; i++)
+        {
+            REQUIRE(crb.pop_nb(&val));
+            REQUIRE(val == i);
+            REQUIRE(crb.getLength() == buffer_size - i);
+        }
+
+        REQUIRE(crb.getLength() == 0);
+        REQUIRE(!crb.pop_nb(&val));
+    }
+
+    SECTION("Test clear")
+    {
+        crb.clear();
+        REQUIRE(crb.getLength() == 0);
+    }
+
+    SECTION("Test multithreading")
+    {
+        int val;
+        size_t length;
+
+        // test waiting to push
+        std::thread push_thread{[&]()
+        {
+            // wait for other thread to pop
+            crb.push(buffer_size);
+            length = crb.getLength();
+        }};
+
+        std::thread pop_thread{[&]()
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            crb.pop(&val);
+        }};
+
+        push_thread.join();
+        pop_thread.join();
+
+        REQUIRE(length == buffer_size);
+        REQUIRE(val == 0);
+
+        // test waiting to pop
+        crb.clear();
+
+        std::thread push_thread2{[&]()
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            crb.push(1);
+        }};
+
+        std::thread pop_thread2{[&]()
+        {
+            // wait for other thread to push
+            crb.pop(&val);
+            length = crb.getLength();
+        }};
+
+        push_thread2.join();
+        pop_thread2.join();
+
+        REQUIRE(length == 0);
+        REQUIRE(val == 1);
     }
 }
