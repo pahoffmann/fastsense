92d8b145c86426286e9e98a8415d6c382e339e14
pahoffmann <pahoffmann@uos.de>
2020-10-16 12:59:26 +0200

    removed commented code

--- test/registration.cpp
+++ test/registration.cpp
@@ -160,46 +160,6 @@ static const std::string error_message =
 TEST_CASE("Registration", "[registration][slow]")
 {
     std::cout << "Testing 'Registration'" << std::endl;
-    // const char* xclbinFilename = "FastSense.xclbin";
-
-    // fs::hw::FPGAManager::load_xclbin(xclbinFilename);
-
-
-
-    // // These commands will allocate memory on the Device. The cl::Buffer objects can
-    // // be used to reference the memory locations on the device.
-    // fs::buffer::InputBuffer<int> buffer_a{q, DATA_SIZE};
-    // fs::buffer::InputBuffer<int> buffer_b{q, DATA_SIZE};
-    // fs::buffer::OutputBuffer<int> buffer_result{q, DATA_SIZE};
-
-    // //setting input data
-    // for (int i = 0 ; i < DATA_SIZE; i++)
-    // {
-    //     buffer_a[i] = 10;
-    //     buffer_b[i] = 20;
-    // }
-
-    // fs::kernels::VaddKernel krnl_vadd{q};
-
-    // krnl_vadd.run(buffer_a, buffer_b, buffer_result, DATA_SIZE);
-    // krnl_vadd.waitComplete();
-
-    // //Verify the result
-    // for (int i = 0; i < DATA_SIZE; i++)
-    // {
-    //     int host_result = buffer_a[i] + buffer_b[i];
-    //     REQUIRE(buffer_result[i] == host_result);
-    // }
-
-
-    // //create a registration object:
-    // Registration registration = new Registration();
-
-    // //use registration object to calculate a specific transformation, for example: create a pointcloud, transform it and calculate the transformation using the registration
-    // //might be a basic cloud with random data - or to make it random, but repeatable: use seeds.
-
-
-    // create pcl from velodyne sample, create local map, transform pcl and see what the reconstruction can do.
 
 
     fastsense::CommandQueuePtr q = fastsense::hw::FPGAManager::create_command_queue();
