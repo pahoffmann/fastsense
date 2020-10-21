9898147d8a696169b92a47b48fbc2a25f068a022
mhillmann <mhillmann@uos.de>
2020-10-21 11:41:44 +0200

    added time measurement to cloud_callback

--- src/callback/cloud_callback.cpp
+++ src/callback/cloud_callback.cpp
@@ -1,32 +1,35 @@
 /**
- * 
+ *
  * @author Pascal Buschermöhle
- * 
+ *
  */
 
 
 #include "cloud_callback.h"
+#include <util/runtime_evaluator.h>
 #include <algorithm>
 
 using namespace fastsense::callback;
+using namespace fastsense::util;
 //using fastsense::buffer::InputOutputBuffer;
 using fastsense::buffer::InputBuffer;
 using fastsense::msg::Point;
 
-CloudCallback::CloudCallback(Registration& registration, const std::shared_ptr<PointCloudBuffer>& cloud_buffer, LocalMap& local_map, const std::shared_ptr<GlobalMap>& global_map, Matrix4f& pose, const std::shared_ptr<TSDFBuffer>& tsdf_buffer, fastsense::CommandQueuePtr& q)
-     : ProcessThread(), 
-     registration{registration},
-     cloud_buffer{cloud_buffer},
-     local_map{local_map},
-     global_map{global_map},
-     pose{pose},
-     tsdf_buffer{tsdf_buffer},
-     first_iteration{true},
-     q{q},
-     tsdf_krnl(q)
-     {
+CloudCallback::CloudCallback(Registration& registration, const std::shared_ptr<PointCloudBuffer>& cloud_buffer, LocalMap& local_map, const std::shared_ptr<GlobalMap>& global_map, Matrix4f& pose,
+                             const std::shared_ptr<TSDFBuffer>& tsdf_buffer, fastsense::CommandQueuePtr& q)
+    : ProcessThread(),
+      registration{registration},
+      cloud_buffer{cloud_buffer},
+      local_map{local_map},
+      global_map{global_map},
+      pose{pose},
+      tsdf_buffer{tsdf_buffer},
+      first_iteration{true},
+      q{q},
+      tsdf_krnl(q)
+{
 
-     }
+}
 
 void CloudCallback::start()
 {
@@ -45,7 +48,7 @@ void CloudCallback::stop()
 void CloudCallback::preprocess_scan(const fastsense::msg::PointCloudStamped& cloud, InputBuffer<PointHW>& scan_points)
 {
     const std::vector<fastsense::msg::Point>& cloud_points = cloud.first->points_;
-    
+
     Eigen::Vector4f v;
 
     int scan_points_index = 0;
@@ -56,15 +59,15 @@ void CloudCallback::preprocess_scan(const fastsense::msg::PointCloudStamped& clo
             continue;
         }
         v << cloud_points[i].x, cloud_points[i].y, cloud_points[i].z, 1.0f;
-        
+
         Vector3f pointf = (pose * v).block<3, 1>(0, 0);
-        PointHW point{static_cast<int>(std::floor(pointf.x())), 
-                      static_cast<int>(std::floor(pointf.y())), 
+        PointHW point{static_cast<int>(std::floor(pointf.x())),
+                      static_cast<int>(std::floor(pointf.y())),
                       static_cast<int>(std::floor(pointf.z()))};
 
         scan_points[scan_points_index] = point;
         scan_points_index++;
-        
+
     }
 }
 
@@ -72,8 +75,8 @@ size_t CloudCallback::determineBufferSize(const fastsense::msg::PointCloudStampe
 {
     const std::vector<fastsense::msg::Point>& cloud_points = cloud.first->points_;
 
-    return std::count_if(cloud_points.begin(), cloud_points.end(), [](const fastsense::msg::Point& p) 
-    {  
+    return std::count_if(cloud_points.begin(), cloud_points.end(), [](const fastsense::msg::Point & p)
+    {
         return p.x != 0 || p.y != 0 || p.z != 0;
     });
 }
@@ -82,39 +85,63 @@ void CloudCallback::callback()
 {
     while (running)
     {
+#ifdef TIME_MEASUREMENT
+        auto& eval = RuntimeEvaluator::get_instance();
+        eval.start("init")
+#endif
+
         fastsense::msg::PointCloudStamped point_cloud;
         cloud_buffer->pop(&point_cloud);
 
         InputBuffer<PointHW> scan_point_buffer{q, determineBufferSize(point_cloud)};
         preprocess_scan(point_cloud, scan_point_buffer);
 
-        // if (first_iteration)
-        // {
-        //     first_iteration = false;
-        // }
-        // else
-        // {
-        //     Matrix4f transform = registration.register_cloud(local_map, scan_point_buffer, q);
-        //     pose = transform * pose;
-        //     int x = (int)std::floor(pose(0, 3));
-        //     int y = (int)std::floor(pose(1, 3));
-        //     int z = (int)std::floor(pose(2, 3));
-        //     local_map.shift(x, y, z);
-        // }
+#ifdef TIME_MEASUREMENT
+        eval.stop()
+#endif
 
-        if (!first_iteration)
+        if (first_iteration)
         {
-            return;
+            first_iteration = false;
+        }
+        else
+        {
+#ifdef TIME_MEASUREMENT
+            eval.start("reg")
+#endif
+
+            Matrix4f transform = registration.register_cloud(local_map, scan_point_buffer, q);
+
+#ifdef TIME_MEASUREMENT
+            eval.stop()
+            eval.start("shift")
+#endif
+
+            pose = transform * pose;
+            int x = (int)std::floor(pose(0, 3));
+            int y = (int)std::floor(pose(1, 3));
+            int z = (int)std::floor(pose(2, 3));
+
+            local_map.shift(x, y, z);
+
+#ifdef TIME_MEASUREMENT
+            eval.stop()
+#endif
         }
 
         int tau = (int)ConfigManager::config().slam.max_distance();
-        
-        std::cout << "Started" << std::endl;
-        
+
+#ifdef TIME_MEASUREMENT
+        eval.start("tsdf")
+#endif
+
         tsdf_krnl.run(local_map, scan_point_buffer, tau, ConfigManager::config().slam.max_weight());
         tsdf_krnl.waitComplete();
 
-        std::cout << "Finished" << std::endl;
+#ifdef TIME_MEASUREMENT
+        eval.stop()
+        eval.start("vis")
+#endif
 
         msg::TSDFBridgeMessage tsdf_msg;
         tsdf_msg.tau_ = tau;
@@ -132,6 +159,9 @@ void CloudCallback::callback()
         std::copy(local_map.getBuffer().cbegin(), local_map.getBuffer().cend(), std::back_inserter(tsdf_msg.tsdf_data_));
         tsdf_buffer->push_nb(tsdf_msg, true);
 
-        first_iteration = false;
+#ifdef TIME_MEASUREMENT
+        eval.stop()
+        std::cout << eval << std::endl;
+#endif
     }
 }
