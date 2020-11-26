7fb127640e86fac25bf8f9f55d0287470dd99bef
gjulian <gjulian@uos.de>
2020-11-26 18:39:41 +0100

    open qemu ports 4444 3333

---
+++ src/comm/buffered_imu_receiver.h
@@ -0,0 +1,18 @@
+//
+// Created by fastsense on 26.11.20.
+//
+
+#pragma once
+
+/**
+ * @file ${HEADER_FILENAME}
+ * @author Julian Gaal
+ */
+
+${NAMESPACES_OPEN}
+
+class buffered_imu_receiver {
+
+};
+
+${NAMESPACES_CLOSE}
