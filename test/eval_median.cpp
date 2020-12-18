499ba881511d0166196e9b47acecf71adf92719f
meisoldt <meisoldt@uni-osnabrueck.de>
2020-12-18 11:57:12 +0100

    Compare with and without preprocessing

--- test/eval_median.cpp
+++ test/eval_median.cpp
@@ -11,10 +11,12 @@
 #include <registration/registration.h>
 #include <util/pcd/pcd_file.h>
 #include <hw/kernels/tsdf_kernel.h>
+#include <preprocessing/preprocessing.h>
 
 #include "catch2_config.h"
 
 using fastsense::util::PCDFile;
+using namespace fastsense::msg;
 
 namespace fastsense::registration
 {
@@ -41,13 +43,21 @@ constexpr int SIZE_X = 20 * SCALE / MAP_RESOLUTION;
 constexpr int SIZE_Y = 20 * SCALE / MAP_RESOLUTION;
 constexpr int SIZE_Z = 5 * SCALE / MAP_RESOLUTION;
 
+struct EvalStats
+{
+    int minimum;
+    int maximum;
+    int average;
+    int median;
+};
+
 /**
  * @brief Compares two sets of points one-on-one and does some statistics
  *
  * @param points_posttransform Transformed point cloud
  * @param points_pretransform Original point cloud
  */
-static void check_computed_transform(const ScanPoints_t& points_posttransform, ScanPoints_t& points_pretransform)
+static EvalStats get_transform_error(const ScanPoints_t& points_posttransform, const ScanPoints_t& points_pretransform)
 {
     int minimum = std::numeric_limits<int>::infinity();
     int maximum = -std::numeric_limits<int>::infinity();
@@ -84,15 +94,7 @@ static void check_computed_transform(const ScanPoints_t& points_posttransform, S
 
     std::sort(dists.begin(), dists.end());
 
-    std::cout << "minimum distance: " << minimum << std::endl;
-    std::cout << "maximum distance: " << maximum << std::endl;
-    std::cout << "average distance: " << average / points_pretransform.size() << std::endl;
-    std::cout << "average distance x: " << average_x / points_pretransform.size() << std::endl;
-    std::cout << "average distance y: " << average_y / points_pretransform.size() << std::endl;
-    std::cout << "average distance z: " << average_z / points_pretransform.size() << std::endl;
-    std::cout << "median distance: " << dists[dists.size() / 2 + 1] << std::endl;
-
-    REQUIRE((average / points_pretransform.size()) < MAX_OFFSET);
+    return {minimum, maximum, average, dists[dists.size() / 2 + 1]};
 }
 
 static std::shared_ptr<fastsense::buffer::InputBuffer<PointHW>> scan_points_to_input_buffer(ScanPoints_t& cloud, const fastsense::CommandQueuePtr q)
@@ -107,6 +109,17 @@ static std::shared_ptr<fastsense::buffer::InputBuffer<PointHW>> scan_points_to_i
     return buffer_ptr;
 }
 
+static EvalStats eval_registration(fastsense::map::LocalMap& local_map, fastsense::registration::Registration& reg, ScanPoints_t pretransformed, const ScanPoints_t original, fastsense::CommandQueuePtr& q)
+{
+    auto buffer_ptr = scan_points_to_input_buffer(pretransformed, q);
+    auto& buffer = *buffer_ptr;
+    auto result_matrix = reg.register_cloud(local_map, buffer, util::HighResTime::now());
+
+    reg.transform_point_cloud(pretransformed, result_matrix);
+    return get_transform_error(pretransformed, original);
+
+}
+
 static const std::string error_message =
     "Error: Result mismatch:\n"
     "i = %d CPU result = %d Device result = %d\n";
@@ -115,6 +128,7 @@ TEST_CASE("Eval_Median", "[eval_median][slow]")
 {
     std::cout << "Testing 'Eval Median'" << std::endl;
 
+    fastsense::preprocessing::Preprocessing preprocessor;
 
     fastsense::CommandQueuePtr q = fastsense::hw::FPGAManager::create_command_queue();
 
@@ -131,10 +145,18 @@ TEST_CASE("Eval_Median", "[eval_median][slow]")
     auto count = 0u;
 
     ScanPoints_t scan_points(num_points);
+    ScanPoints_t scan_points_preprocessed(num_points);
 
     auto q2 = fastsense::hw::FPGAManager::create_command_queue();
     fastsense::buffer::InputBuffer<PointHW> kernel_points(q2, num_points);
 
+    Stamped<PointCloud::Ptr> cloud_stamped;
+    cloud_stamped.data_ = std::make_shared<PointCloud>();
+    cloud_stamped.data_->points_.resize(num_points);
+    cloud_stamped.data_->rings_ = float_points.size();
+
+
+    // Prepare normal registartion points
     for (const auto& ring : float_points)
     {
         for (const auto& point : ring)
@@ -142,6 +164,8 @@ TEST_CASE("Eval_Median", "[eval_median][slow]")
             scan_points[count].x() = point.x() * SCALE;
             scan_points[count].y() = point.y() * SCALE;
             scan_points[count].z() = point.z() * SCALE;
+
+            cloud_stamped.data_->points_[count] = scan_points[count].cast<ScanPointType>();
 
             kernel_points[count].x = scan_points[count].x();
             kernel_points[count].y = scan_points[count].y();
@@ -151,9 +175,20 @@ TEST_CASE("Eval_Median", "[eval_median][slow]")
         }
     }
 
+    // Preprocessed points
+    preprocessor.median_filter(cloud_stamped, 5);
+
+    for (auto index = 0u; index < num_points; ++index)
+    {
+        scan_points_preprocessed[index] = cloud_stamped.data_->points_[index].cast<int>();
+    }
+
     ScanPoints_t scan_points_2(scan_points);
     ScanPoints_t points_pretransformed_trans(scan_points);
     ScanPoints_t points_pretransformed_rot(scan_points);
+
+    ScanPoints_t points_pretransformed_trans_preprocessed(scan_points_preprocessed);
+    ScanPoints_t points_pretransformed_rot_preprocessed(scan_points_preprocessed);
 
     std::shared_ptr<fastsense::map::GlobalMap> global_map_ptr(new fastsense::map::GlobalMap("test_global_map.h5", 0.0, 0.0));
 
@@ -181,48 +216,33 @@ TEST_CASE("Eval_Median", "[eval_median][slow]")
     krnl.run(local_map, kernel_points, TAU, MAX_WEIGHT);
     krnl.waitComplete();
 
-    //fastsense::tsdf::update_tsdf(scan_points, Vector3i::Zero(), local_map, TAU, MAX_WEIGHT);
-
     SECTION("Test Registration No Transform")
     {
         std::cout << "    Section 'Test Registration No Transform'" << std::endl;
-
-        //copy from scanpoints to  inputbuffer
-        auto buffer_ptr = scan_points_to_input_buffer(points_pretransformed_trans, q);
-        auto& buffer = *buffer_ptr;
-        auto result_matrix = reg.register_cloud(local_map, buffer, util::HighResTime::now());
-
-        reg.transform_point_cloud(points_pretransformed_trans, result_matrix);
-        check_computed_transform(points_pretransformed_trans, scan_points);
-
+        auto without = eval_registration(local_map, reg, points_pretransformed_trans, scan_points, q);
+        auto with = eval_registration(local_map, reg, points_pretransformed_trans_preprocessed, scan_points, q);
+    
+        REQUIRE(without.average > with.average);
     }
 
     SECTION("Test Registration Translation")
     {
         std::cout << "    Section 'Test Registration Translation'" << std::endl;
-
         reg.transform_point_cloud(points_pretransformed_trans, translation_mat);
-
-        //copy from scanpoints to  inputbuffer
-        auto buffer_ptr = scan_points_to_input_buffer(points_pretransformed_trans, q);
-        auto& buffer = *buffer_ptr;
-        auto result_matrix = reg.register_cloud(local_map, buffer, util::HighResTime::now());
-
-        reg.transform_point_cloud(points_pretransformed_trans, result_matrix);
-        check_computed_transform(points_pretransformed_trans, scan_points);
-
+        auto without = eval_registration(local_map, reg, points_pretransformed_trans, scan_points, q);
+        auto with = eval_registration(local_map, reg, points_pretransformed_trans_preprocessed, scan_points, q);
+    
+        REQUIRE(without.average > with.average);
     }
 
     SECTION("Registration test Rotation")
     {
         std::cout << "    Section 'Registration test Rotation'" << std::endl;
         reg.transform_point_cloud(points_pretransformed_rot, rotation_mat);
-        auto buffer_ptr = scan_points_to_input_buffer(points_pretransformed_rot, q);
-        auto& buffer = *buffer_ptr;
-        auto result_matrix = reg.register_cloud(local_map, buffer, util::HighResTime::now());
-
-        reg.transform_point_cloud(points_pretransformed_rot, result_matrix);
-        check_computed_transform(points_pretransformed_rot, scan_points_2);
+        auto without = eval_registration(local_map, reg, points_pretransformed_rot, scan_points, q); 
+        auto with = eval_registration(local_map, reg, points_pretransformed_rot_preprocessed, scan_points, q);
+    
+        REQUIRE(without.average > with.average);
     }
 }
 
