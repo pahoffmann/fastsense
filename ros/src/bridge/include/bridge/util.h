1454b3f329b79ccdcd1296d1b25f28c087d5c2f1
mhillmann <mhillmann@uos.de>
2021-01-06 14:15:18 +0100

    fixed from_bridge truncating Milliseconds

--- ros/src/bridge/include/bridge/util.h
+++ ros/src/bridge/include/bridge/util.h
@@ -8,10 +8,13 @@ namespace fastsense::bridge
 
 inline ros::Time timestamp_to_rostime(const fastsense::util::HighResTimePoint& ts)
 {
-    auto t_since_epoch = ts.time_since_epoch();
-    auto sec = std::chrono::duration_cast<std::chrono::seconds>(t_since_epoch).count();
-    auto nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(t_since_epoch).count() % 1'000'000;
-    return ros::Time(sec, nsec);
+    std::chrono::nanoseconds t_since_epoch{ts.time_since_epoch()};
+    int64_t nsec{t_since_epoch.count()};
+    nsec %= 1'000'000'000ll;
+    //      s  ms  µs  ns
+
+    uint32_t sec = std::chrono::duration_cast<std::chrono::seconds>(t_since_epoch).count();
+    return ros::Time(sec, static_cast<uint32_t>(nsec));
 }
 
 }
