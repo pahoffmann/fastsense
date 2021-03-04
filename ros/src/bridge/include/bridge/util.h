97160b1d3b070f8297fdc3c10cc49b57e8c37502
juliangaal <gjulian@uos.de>
2021-03-04 15:45:00 +0100

    simplify timestamp conversion

--- ros/src/bridge/include/bridge/util.h
+++ ros/src/bridge/include/bridge/util.h
@@ -8,13 +8,10 @@ namespace fastsense::bridge
 
 inline ros::Time timestamp_to_rostime(const fastsense::util::HighResTimePoint& ts)
 {
-    std::chrono::nanoseconds t_since_epoch{ts.time_since_epoch()};
-    int64_t nsec{t_since_epoch.count()};
-    nsec %= 1'000'000'000ll;
-    //      s  ms  µs  ns
-
-    uint32_t sec = std::chrono::duration_cast<std::chrono::seconds>(t_since_epoch).count();
-    return ros::Time(sec, static_cast<uint32_t>(nsec));
+	auto t_since_epoch = ts.time_since_epoch();
+	int32_t sec = std::chrono::duration_cast<std::chrono::seconds>(t_since_epoch).count();
+	int32_t nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(t_since_epoch).count() % 1'000'000'000ll;
+	return ros::Time(sec, nsec);
 }
 
 }
